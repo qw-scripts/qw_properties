7ee90d4f3e602b336b92e79c0b0f9e03b4b0de32
masonschafercodes <57614646+masonschafercodes@users.noreply.github.com>
2024-08-24 16:15:00 -0400

    chore: updating object spawning function

--- modules/client/objects.lua
+++ modules/client/objects.lua
@@ -1,11 +1,7 @@
 local obj = {}
 
 function obj.createObjectForPlacement(hash, currentCameraLookAt)
-    local underPlayer = GetOffsetFromEntityInWorldCoords(cache.ped, 0.0, 0.0, -1.0)
-    local objectSpawnPos = underPlayer + (GetEntityForwardVector(cache.ped) * 3.0)
-    local currentObject = CreateObject(hash, objectSpawnPos.x, objectSpawnPos.y, objectSpawnPos.z, false, true, false)
-    SetEntityCoords(currentObject, currentCameraLookAt.x, currentCameraLookAt.y, currentCameraLookAt.z, false, false, false, false)
-
+    local currentObject = CreateObject(hash, currentCameraLookAt.x, currentCameraLookAt.y, currentCameraLookAt.z, false, true, false)
     return currentObject
 end
 
